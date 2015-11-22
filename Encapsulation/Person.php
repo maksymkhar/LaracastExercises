9c99d5f66bc6363aa5f20e8bfc26cea41e9a2eac
Max <maksymkharuk@iesebre.com>
2015-11-22 20:06:16 +0100

    Added encapsulation exercise

---
+++ Encapsulation/Person.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 22/11/15
+ * Time: 19:57
+ */
+
+class Person
+{
+    private $name;
+    private $age;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAge($age)
+    {
+        if ($age < 18)
+        {
+            throw new Exception("Person is not old enough.");
+        }
+
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age * 365;
+    }
+}
+
+$john = new Person('John Doe');
+$john->setAge(18);
+
+//$john->age = 3;
+
+var_dump($john->getAge());
---
+++ Encapsulation/Switch.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 22/11/15
+ * Time: 20:00
+ */
+
+class LightSwitch {
+
+    public function on()
+    {
+
+    }
+
+    public function off()
+    {
+
+    }
+
+    private function connect()
+    {
+
+    }
+
+    private function activate()
+    {
+
+    }
+}
+
+$switch = new LightSwitch;
+//$switch->connect();
